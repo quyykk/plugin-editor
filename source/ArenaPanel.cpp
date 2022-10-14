20110d5004a1b3ff2d0228e16729dfb84462a166
quyykk <quyykk@protonmail.com>
2022-10-14 12:19:05 +0200

    Fix copyright.

--- source/ArenaPanel.cpp
+++ source/ArenaPanel.cpp
@@ -1,6 +1,6 @@
 /* ArenaPanel.cpp
 Copyright (c) 2014 by Michael Zahniser
-Copyright (c) 2022 by Michael Zahniser
+Copyright (c) 2022 by quyykk
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
